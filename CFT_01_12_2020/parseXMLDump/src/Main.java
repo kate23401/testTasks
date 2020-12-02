948f5c59e93cc14180a97f5088f3d0972f0da8e4
Ekaterina Volosnikova <kate234011@gmail.com>
2020-12-02 14:25:48 +0700

    01.12.2020. CFT: improve performance commit#1

--- CFT_01_12_2020/parseXMLDump/src/Main.java
+++ CFT_01_12_2020/parseXMLDump/src/Main.java
@@ -1,5 +1,6 @@
 import java.io.*;
-import java.util.Map;
+import java.time.LocalTime;
+import java.util.ArrayList;
 import org.xml.sax.SAXException;
 import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.parsers.SAXParser;
@@ -7,36 +8,60 @@ import javax.xml.parsers.SAXParserFactory;
 
 public class Main {
     public static void main(String[] args) throws ParserConfigurationException, SAXException, IOException {
-        System.out.println("Введите слово для поиска в консоль." + "\n"
-                + "Чтобы завершить обработку, введите пустое значение или пробел." + "\n"
-                + "------->");
-        // Вычитаем из консоли введенное слово
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        while (true) {
-            String str = reader.readLine().trim();
-            if (str.isEmpty()) {
-                System.out.println("Введено пустое значение, обработка завершается...");
-                break;
-            }
-            // Распарсим файл с учетом переданного из консоли слова
-            SAXParserFactory factory = SAXParserFactory.newInstance();
-            SAXParser parser = factory.newSAXParser();
-            XMLHandler handler = new XMLHandler(str);       // Передаем в конструктор слово, полученное из консоли
-            parser.parse(new File("data/enwiki-latest-abstract.xml"), handler); // Имена файлов: enwiki-latest-abstract, fileXML
+        ArrayList<String> resultList = new ArrayList<>();
 
-            // Проверим нашли ли вхождение слова из консоли
-            if (handler.parsedPair.isEmpty()) {
-                System.out.println("Не найдены совпадения по ключевому слову - " + str + ".\n" + "Измените текст запроса.");
-                continue;   // Переходим на новую итерацию цикла.
-            } else {
-                System.out.println("Найдены совпадения по ключевому слову - " + str);
+        // Вычитали файл при первом запуске программы в HashMap
+        System.out.println("Вычитываем файл в HashMap - " + LocalTime.now() + "\n"
+                + "Дождитесь окончания записи файла.");
+
+        File file = new File("data/enwiki-latest-abstract.xml");
+        SAXParserFactory factory = SAXParserFactory.newInstance();
+        SAXParser parser = factory.newSAXParser();
+        // Складываем файл в HashMap, где ключ title, значение - abstract
+        XMLHandler handler = new XMLHandler();
+        parser.parse(file, handler);
+
+        System.out.println("Файл записан в HashMap - " + LocalTime.now());
+        System.out.println();
+
+        if (file.exists()) {
+            System.out.println("Введите слово для поиска в консоль." + "\n"
+                    + "Чтобы завершить обработку, введите пустое значение или пробел." + "\n");
+
+            // Вычитаем из консоли введенное слово
+            BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+            while (true) {
+                String str = reader.readLine().trim();
+                if (str.isEmpty()) {
+                    System.out.println("Введено пустое значение, обработка завершается...");
+                    break;
+                }
+
+                System.out.println("Поиск совпадений по введенному слову запущен - " + LocalTime.now());
+                resultList = handler.checkMatchesInNode(str);
+                System.out.println("Поиск совпадений по введенному слову завершен - " + LocalTime.now());
+
+                // Проверим нашли ли вхождение слова из консоли
+                if (resultList.isEmpty()) {
+                    System.out.println("Не найдены совпадения по ключевому слову - " + str + ".\n" + "Измените текст запроса.");
+                    continue;   // Переходим на новую итерацию цикла.
+                } else {
+                    System.out.println("Найдены совпадения по ключевому слову - " + str);
+                }
+
+                // Выведем найденные пары title-abstract
+                resultList.forEach((k) -> System.out.println(k + "\n"));
+                // Очищаем выборку, чтобы данные не анализировались в следующей итерации
+                resultList.clear();
+
             }
-            // Выведем найденные пары title-abstract
-            for (Map.Entry<String, String> stringStringEntry : handler.parsedPair.entrySet()) {
-                System.out.println(stringStringEntry.getKey() + "\n" + stringStringEntry.getValue());
-            }
-            // Очищаем выборку, чтобы данные не анализировались в следующей итерации
-            handler.parsedPair.clear();
+        } else {
+            System.out.println("Файл с именем не найден в папке data!" + "\n"
+                    + " Убедитесь, что файл с именем лежит в директории.");
         }
+
+        System.out.println("Завершение работы поиска слова - " + LocalTime.now());
+        // Очищаем массив c файлом после завершения поиска
+        handler.parsedPair.clear();
     }
 }
