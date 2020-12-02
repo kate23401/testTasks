d23784835c423477cfa8c95b0c669c7cce9a25be
Ekaterina Volosnikova <kate234011@gmail.com>
2020-12-02 23:46:07 +0700

    01.12.2020. CFT: improve performance commit#2

--- CFT_01_12_2020/parseXMLDump/src/Main.java
+++ CFT_01_12_2020/parseXMLDump/src/Main.java
@@ -1,6 +1,5 @@
 import java.io.*;
 import java.time.LocalTime;
-import java.util.ArrayList;
 import org.xml.sax.SAXException;
 import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.parsers.SAXParser;
@@ -8,7 +7,6 @@ import javax.xml.parsers.SAXParserFactory;
 
 public class Main {
     public static void main(String[] args) throws ParserConfigurationException, SAXException, IOException {
-        ArrayList<String> resultList = new ArrayList<>();
 
         // Вычитали файл при первом запуске программы в HashMap
         System.out.println("Вычитываем файл в HashMap - " + LocalTime.now() + "\n"
@@ -26,7 +24,7 @@ public class Main {
 
         if (file.exists()) {
             System.out.println("Введите слово для поиска в консоль." + "\n"
-                    + "Чтобы завершить обработку, введите пустое значение или пробел." + "\n");
+                    + "Чтобы завершить обработку, введите пустое значение или пробел.");
 
             // Вычитаем из консоли введенное слово
             BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
@@ -37,27 +35,16 @@ public class Main {
                     break;
                 }
 
-                System.out.println("Поиск совпадений по введенному слову запущен - " + LocalTime.now());
-                resultList = handler.checkMatchesInNode(str);
-                System.out.println("Поиск совпадений по введенному слову завершен - " + LocalTime.now());
-
-                // Проверим нашли ли вхождение слова из консоли
-                if (resultList.isEmpty()) {
+                if (!handler.parsedPair.containsKey(str)) {
                     System.out.println("Не найдены совпадения по ключевому слову - " + str + ".\n" + "Измените текст запроса.");
-                    continue;   // Переходим на новую итерацию цикла.
                 } else {
-                    System.out.println("Найдены совпадения по ключевому слову - " + str);
+                    System.out.println("Поиск по запрашиваемому слову " + str + " запущен - " + LocalTime.now() + ".\n");
+                    System.out.println(handler.parsedPair.get(str));
                 }
-
-                // Выведем найденные пары title-abstract
-                resultList.forEach((k) -> System.out.println(k + "\n"));
-                // Очищаем выборку, чтобы данные не анализировались в следующей итерации
-                resultList.clear();
-
             }
         } else {
-            System.out.println("Файл с именем не найден в папке data!" + "\n"
-                    + " Убедитесь, что файл с именем лежит в директории.");
+            System.out.println("Файл с именем enwiki-latest-abstract.xml не найден в папке data!" + "\n"
+                    + " Убедитесь, что файл лежит в директории.");
         }
 
         System.out.println("Завершение работы поиска слова - " + LocalTime.now());
